e8d7d253b71f8831356e7200dfc958a29c5c1827
Paulo Jerônimo <paulojeronimo@gmail.com>
2019-02-20 13:21:52 +0000

    Added support to show principal name

--- src/main/java/com/example/demo/HelloEndpoint.java
+++ src/main/java/com/example/demo/HelloEndpoint.java
@@ -4,19 +4,21 @@ import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 public class HelloEndpoint {
 
     @GetMapping("/admin/hello")
     @Secured("ROLE_ADMIN")
-    public String sayHelloToAdmin() {
-        return "Hello Admin";
+    public String sayHelloToAdmin(final Principal principal) {
+        return "Hello Admin: " + principal.getName();
     }
 
     @GetMapping("/user/hello")
     @Secured("ROLE_USER")
-    public String sayHelloToUser() {
-        return "Hello User";
+    public String sayHelloToUser(final Principal principal) {
+        return "Hello User: " + principal.getName();
     }
 
     @GetMapping("/guest/hello")
